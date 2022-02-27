84d74f05d4ca8dd962a3932a97ec54ff3950b9f0
furquan08 <32553085+furquan08@users.noreply.github.com>
2022-02-27 13:18:03 +0530

    changes in web client

--- src/main/java/com/jos/dem/springboot/cucumber/CucumberApplication.java
+++ src/main/java/com/jos/dem/springboot/cucumber/CucumberApplication.java
@@ -1,21 +1,32 @@
 package com.jos.dem.springboot.cucumber;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class CucumberApplication {
 
+    @Value("${store.boundary.service.url}")
+    private String sbsUrl;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(CucumberApplication.class, args);
   }
 
   @Bean
   WebClient getWebClient() {
-    return WebClient.create("http://services-mi-qa1.us.dominos.com:8080/store-boundary-service");
+      return WebClient
+              .builder()
+              .baseUrl(sbsUrl)
+              .defaultHeader("DPZ-Market", "UNITED_STATES")
+              .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+              .build();
   }
 
 }
